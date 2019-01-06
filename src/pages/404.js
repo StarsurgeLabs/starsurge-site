c67682ca558998fe3d440f449c91e9e7b369d08a
Leandro Alexandrino <code@leandrino.me>
2019-01-05 22:19:14 -0200

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
+export default () => <Wrapper>Página não encontrada!</Wrapper>;
