678e035b727bff9ed867bf355589e88910e63cbf
Leandro Alexandrino <code@leandrino.me>
2019-01-08 14:44:27 -0200

    start home styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,19 @@
 import React from "react";
+import styled from "@emotion/styled";
 import FullScreen from "../full-screen";
 
+const Wrapper = styled(FullScreen.header)`
+  background-color: #000060;
+  color: #f5f5f5;
+`;
+
 const Header = () => (
-  <FullScreen.header>
+  <Wrapper>
     <FullScreen.content>
       <h1>Starsurge Labs</h1>
       <p>O início de seu novo projeto, começa aqui!</p>
     </FullScreen.content>
-  </FullScreen.header>
+  </Wrapper>
 );
 
 export default Header;
