cb73359c0a7e0784adc6b5a1e25757f387e97230
Leandro Alexandrino <code@leandrino.me>
2019-01-11 22:08:54 -0200

    settigns update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  siteMetadata: {
+    title: "Starsurge Labs",
+    siteUrl: "https://starsurge.space",
+    description: "Seu projeto web começa aqui!"
+  },
   plugins: [
     {
       resolve: "gatsby-plugin-typography",
